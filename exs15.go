703e479a9b67f7918b70a6bc2653c04aecf3d0c5
gabigguti <119536472+gabigguti@users.noreply.github.com>
2023-09-30 14:06:17 -0300

    Add files via upload

---
+++ exs15.go
@@ -0,0 +1,19 @@
+package main
+
+package main
+
+import ( "fmt", "strings" )
+
+func main() {
+	var palavra string
+	fmt.Println("Digite uma palavra:")
+	fmt.Scan(&palavra)
+
+	palavra = strings.ReplaceAll(palavra, "a", "*")
+	palavra = strings.ReplaceAll(palavra, "e", "*")
+	palavra = strings.ReplaceAll(palavra, "i", "*")
+	palavra = strings.ReplaceAll(palavra, "o", "*")
+	palavra = strings.ReplaceAll(palavra, "u", "*")
+
+	fmt.Println(palavra)
+}
---
+++ exs8.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	var str string
+	fmt.Print("Digite uma string: ")
+	fmt.Scanln(&str)
+
+	reversed := ""
+	for i := len(str) - 1; i >= 0; i-- {
+		reversed += string(str[i])
+	}
+
+	fmt.Printf("A string \"%s\" invertida é \"%s\".\n", str, reversed)
+}
